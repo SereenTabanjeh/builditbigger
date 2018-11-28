691215c0cf673bb6cb86b90397aa1a5dbc54dc6a
SereenTabanjeh <sereen90.tabanjeh@gmail.com>
2018-11-28 21:53:32 +0200

    Update EndpointsAsyncTask.java

--- app/src/main/java/com/udacity/gradle/builditbigger/EndpointsAsyncTask.java
+++ app/src/main/java/com/udacity/gradle/builditbigger/EndpointsAsyncTask.java
@@ -15,61 +15,7 @@ import com.udacity.gradle.builditbigger.backend.myApi.MyApi;
 
 
 import java.io.IOException;
-/*
-public class EndpointsAsyncTask extends AsyncTask<MainActivityFragment, Void, String> {
-    private static MyApi myApiService = null;
-    private Context mContext;
-    public String loadedJoke = null;
-    private MainActivityFragment mainActivityFragment;
 
-    public EndpointsAsyncTask(Context context){
-        mContext = context;
-    }
-    @Override
-    protected String doInBackground(MainActivityFragment... params) {
-        if(myApiService == null) {  // Only do this once
-            MyApi.Builder builder = new MyApi.Builder(AndroidHttp.newCompatibleTransport(),
-                    new AndroidJsonFactory(), null)
-                    // options for running against local devappserver
-                    // - 10.0.2.2 is localhost's IP address in Android emulator
-                    // - turn off compression when running against local devappserver
-//                    .setRootUrl("http://10.0.2.2:8889/_ah/api/")
-                    //For remote Backend
-                    .setRootUrl("https://builditbigger-9001500.appspot.com/_ah/api/")
-                    .setGoogleClientRequestInitializer(new GoogleClientRequestInitializer() {
-                        @Override
-                        public void initialize(AbstractGoogleClientRequest<?> abstractGoogleClientRequest) throws IOException {
-                            abstractGoogleClientRequest.setDisableGZipContent(true);
-                        }
-                    });
-            // end options for devappserver
-
-            myApiService = builder.build();
-        }
-
-        //context = params[0].first;
-          mainActivityFragment = params[0];
-        mContext = mainActivityFragment.getActivity();
-
-        try {
-            return myApiService.sayHi().execute().getData();
-        } catch (IOException e) {
-            return null;
-        }
-    }
-
-
-
-    @Override
-    protected void onPostExecute(String result) {
-
-        mainActivityFragment.loadedJoke = result;
-        mainActivityFragment.launchDisplayJokeActivity();
-
-
-    }
-}
-*/
 
 class EndpointsAsyncTask extends AsyncTask<MainActivityFragment, Void, String> {
     private static final String TAG = "EndpointAsyncTask";
